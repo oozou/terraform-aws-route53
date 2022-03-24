8a510a1d1a27cf013e74f08c87ed15c2af3de1ae
sedthawut tipkanpirome <sedthawut2000@gmail.com>
2022-03-25 01:12:54 +0700

    (update): output format

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
+output "route53_name" {
+  description = "Name of Route53 zone"
+  value       = element(concat(aws_route53_zone.this[*].name, [""]), 0)
+}
+
 output "route53_zone_id" {
   description = "Zone ID of Route53 zone"
-  value       = { for k, v in aws_route53_zone.this : k => v.zone_id }
+  value       = element(concat(aws_route53_zone.this[*].zone_id, [""]), 0)
 }
 
 output "route53_name_servers" {
   description = "Name servers of Route53 zone"
-  value       = { for k, v in aws_route53_zone.this : k => v.name_servers }
-}
-
-output "route53_name" {
-  description = "Name of Route53 zone"
-  value       = { for k, v in aws_route53_zone.this : k => v.name }
+  value       = element(concat(aws_route53_zone.this[*].name_servers, [""]), 0)
 }
 
 output "route53_record_name" {
